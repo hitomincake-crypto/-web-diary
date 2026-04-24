9e4b370c77a64da36a2751c977bb3c3d791d668d
hitomincake-crypto <hitomincake@gmail.com>
2026-04-25 01:44:03 +0900

    Add files via upload

--- template-mobile.js
+++ template-mobile.js
@@ -1,16 +1,8 @@
 export function diaryItemTemplateMobile(d, data, formatTime) {
-  const nick = data.nickname || "";
-  const time = formatTime(data.createdAt);
-  const titleText = data.title || "(無題)";
+const nick = data.nickname || "";
+const time = formatTime(data.createdAt);
+const titleText = data.title || "(無題)";
 
-  return `
-    <div class="nickname">${nick}<br>${time}</div>
-    <h4>${titleText}</h4>
-    <div class="actions">
-      <button onclick="viewDiary('${d.id}')">👁</button>
-      <button onclick="editDiary('${d.id}')">✏️</button>
-      <button onclick="commentDiary('${d.id}')">💬</button>
-      <button onclick="deleteDiary('${d.id}')">🗑</button>
-    </div>
+return `     <div class="nickname">${nick}<br>${time}</div>     <h4>${titleText}</h4>     <div class="actions">       <button onclick="viewDiary('${d.id}')">見る</button>       <button onclick="editDiary('${d.id}')">編集</button>       <button onclick="commentDiary('${d.id}')">コメ</button>       <button onclick="deleteDiary('${d.id}')">削除</button>     </div>
   `;
 }
