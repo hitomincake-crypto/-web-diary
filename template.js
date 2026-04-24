bf92b57dab2cbcaa482652f8781ffb7cb16cc7f0
hitomincake-crypto <hitomincake@gmail.com>
2026-04-25 01:06:50 +0900

    Add files via upload

--- template.js
+++ template.js
@@ -3,7 +3,7 @@ import { diaryItemTemplateMobile } from "./template-mobile.js";
 
 // 画面サイズ判定
 function isMobile() {
-  return window.innerWidth <= 768;
+  return window.innerWidth <= 1020;
 }
 
 // 🔥 既存関数をそのまま使う（これが重要）
